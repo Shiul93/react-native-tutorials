1ffd4d6617f088da4d23e8576ae50f02c20b4aca
Shiul93 <lfllamas93@gmail.com>
2018-04-19 11:40:36 +0200

    Adding files of the official tutorial

---
+++ AwesomeProject/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Bananas from './src/Bananas';
+import LotsOfGreetings from './src/Greeting';
+import Blink from './src/Blink';
+import LotsOfStyles from './src/LotsOfStyles';
+import FixedDimensionsBasics from './src/FixedDimensionsBasics';
+import FlexDimensionsBasics from './src/FlexDimensionsBasics';
+import FlexDirectionBasics from './src/FlexDirectionBasics';
+import JustifyContentsBasics from './src/JustifyContentsBasics';
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      
+        
+        <JustifyContentsBasics></JustifyContentsBasics>
+      
+    );
+  }
+}
